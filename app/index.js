21096edb7863fedcc6f54a70a419cf1110659b07
unknown <h-urayama@h-urayama-note.ds.iiji.jp>
2014-03-23 15:44:06 +0900

    add options

--- app/index.js
+++ app/index.js
@@ -5,8 +5,11 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 
 
-var SasatSiteBaseProvider = yeoman.generators.NamedBase.extend({
+var SasatSiteBaseProvider = yeoman.generators.Base.extend({
   init: function () {
+    this.argument('name', { type: String, required: true });
+    this.argument('createSP', { type: String, required: true });
+
     this.pkg = require('../package.json');
 
     this.on('end', function () {
@@ -19,7 +22,10 @@ var SasatSiteBaseProvider = yeoman.generators.NamedBase.extend({
   app: function () {
     var data = {name: this.name};
     this.template('base.html', 'out.html', data);
-    
+    if (this.createSP === 'true') {
+      this.template('base_sp.html', 'out_sp.html');
+    }
+
     this.copy('_package.json', 'package.json');
     this.copy('_bower.json', 'bower.json');
   },
